062119afc25d56cc540c8d34f35f3b163948de01
cassius23 <emilekm@gmail.com>
2023-12-08 08:23:48 +0100

    Adjust config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	Murmur Murmur `yaml:"murmur"`
-	Prbf2  PRBF2  `yaml:"prbf2"`
+	PRBF2  PRBF2  `yaml:"prbf2"`
 }
 
 type Murmur struct {
@@ -34,6 +34,24 @@ type Squads struct {
 	KickAFKPercent       float64 `yaml:"kickAFKPercent"`
 	KickSquadedAFK       bool    `yaml:"kickSquadedAFK"`
 	KickSquadedAFKTime   int     `yaml:"kickSquadedAFKTime"`
+}
+
+type Admin struct {
+	Name  string `yaml:"name"`
+	Hash  string `yaml:"hash"`
+	Level int    `yaml:"level"`
+}
+
+type PrismUser struct {
+	Name     string `yaml:"name"`
+	Password string `yaml:"password"`
+	Level    int    `yaml:"level"`
+}
+
+type Maplist struct {
+	Name  string `yaml:"name,omitempty"`
+	Layer string `yaml:"layer,omitempty"`
+	Mode  string `yaml:"mode"`
 }
 
 type CommandLevels struct {
@@ -88,28 +106,28 @@ type CommandLevels struct {
 }
 
 type PRBF2 struct {
-	Ports            Ports           `yaml:"ports"`
-	Countryflag      string          `yaml:"countryflag"`
-	IP               string          `yaml:"ip"`
-	ExternalIP       string          `yaml:"externalIP"`
-	Name             string          `yaml:"name"`
-	Password         string          `yaml:"password"`
-	Internet         int             `yaml:"internet"`
-	SponsorLogoURL   string          `yaml:"sponsorLogoURL"`
-	CommunityLogoURL string          `yaml:"communityLogoURL"`
-	MaxPlayers       int             `yaml:"maxPlayers"`
-	VotingEnabled    int             `yaml:"votingEnabled"`
-	DemoQuality      int             `yaml:"demoQuality"`
-	SponsoreMessage  SponsoreMessage `yaml:"sponsoreMessage"`
-	Squads           Squads          `yaml:"squads"`
-	Admins           []interface{}   `yaml:"admins"`
-	LiteAdmins       []interface{}   `yaml:"liteAdmins"`
-	PrismUsers       []interface{}   `yaml:"prismUsers"`
-	ReservedSlotsNum int             `yaml:"reservedSlotsNum"`
-	ReservedSlots    []interface{}   `yaml:"reservedSlots"`
-	Maplist          []interface{}   `yaml:"maplist"`
-	AllMaps          []interface{}   `yaml:"allMaps"`
-	CommandAliases   interface{}     `yaml:"commandAliases"`
-	Reasons          interface{}     `yaml:"reasons"`
-	CommandLevels    CommandLevels   `yaml:"commandLevels"`
+	Ports            Ports             `yaml:"ports"`
+	Countryflag      string            `yaml:"countryflag"`
+	IP               string            `yaml:"ip"`
+	ExternalIP       string            `yaml:"externalIP"`
+	Name             string            `yaml:"name"`
+	Password         string            `yaml:"password"`
+	Internet         int               `yaml:"internet"`
+	SponsorLogoURL   string            `yaml:"sponsorLogoURL"`
+	CommunityLogoURL string            `yaml:"communityLogoURL"`
+	MaxPlayers       int               `yaml:"maxPlayers"`
+	VotingEnabled    int               `yaml:"votingEnabled"`
+	DemoQuality      int               `yaml:"demoQuality"`
+	SponsoreMessage  SponsoreMessage   `yaml:"sponsoreMessage"`
+	Squads           Squads            `yaml:"squads"`
+	Admins           []Admin           `yaml:"admins"`
+	LiteAdmins       []Admin           `yaml:"liteAdmins"`
+	PrismUsers       []PrismUser       `yaml:"prismUsers"`
+	ReservedSlotsNum int               `yaml:"reservedSlotsNum"`
+	ReservedSlots    []string          `yaml:"reservedSlots"`
+	Maplist          []Maplist         `yaml:"maplist"`
+	AllMaps          []interface{}     `yaml:"allMaps"`
+	CommandAliases   map[string]string `yaml:"commandAliases"`
+	Reasons          map[string]string `yaml:"reasons"`
+	CommandLevels    CommandLevels     `yaml:"commandLevels"`
 }
