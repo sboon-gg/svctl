e2808415d5b80f6dab40ed8ba20afdfdb4956dff
cassius23 <emilekm@gmail.com>
2024-03-25 17:49:13 +0000

    Small refactor

--- pkg/prbf2update/cache.go
+++ pkg/prbf2update/cache.go
@@ -15,6 +15,11 @@ import (
 	"github.com/hashicorp/go-version"
 )
 
+const (
+	latestVersionUrl = "http://prbf2.cdn.ancientdev.com/version.json"
+	patchMetaUrlFmt  = "http://prbf2.cdn.ancientdev.com/patch_%s.json"
+)
+
 var caches = make(map[string]*Cache)
 
 type Cache struct {
@@ -43,12 +48,7 @@ func (c *Cache) FetchFor(currentVersion, requiredVersion string) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
-	latestVersion, err := c.LatestVersion()
-	if err != nil {
-		return err
-	}
-
-	if latestVersion == currentVersion {
+	if requiredVersion == currentVersion {
 		return nil
 	}
 
@@ -60,7 +60,7 @@ func (c *Cache) FetchFor(currentVersion, requiredVersion string) error {
 	var patchFilePaths []string
 
 	for {
-		info, err := c.patchInfo(requiredVersion)
+		info, err := c.patchMeta(requiredVersion)
 		if err != nil {
 			return err
 		}
@@ -115,22 +115,15 @@ func (c *Cache) FetchFor(currentVersion, requiredVersion string) error {
 }
 
 func (c *Cache) LatestVersion() (string, error) {
-	versionUrl := "http://prbf2.cdn.ancientdev.com/version.json"
-	resp, err := http.Get(versionUrl)
+	content, err := httpGet(latestVersionUrl)
 	if err != nil {
 		return "", err
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to fetch version.json: %s", resp.Status)
-	}
-
-	defer resp.Body.Close()
-
 	latestVersion := struct {
 		Latest string `json:"Latest"`
 	}{}
-	if err := json.NewDecoder(resp.Body).Decode(&latestVersion); err != nil {
+	if err := json.Unmarshal(content, &latestVersion); err != nil {
 		return "", fmt.Errorf("failed to decode version.json: %w", err)
 	}
 
@@ -138,13 +131,11 @@ func (c *Cache) LatestVersion() (string, error) {
 }
 
 func (c *Cache) cachePatch(patchUrl string) (string, error) {
-	u, err := url.Parse(patchUrl)
+	filename, err := filenameFromURL(patchUrl)
 	if err != nil {
 		return "", err
 	}
 
-	parts := strings.Split(u.Path, "/")
-	filename := parts[len(parts)-1]
 	patchPath := filepath.Join(c.path, filename)
 
 	if _, err := os.Stat(patchPath); err == nil {
@@ -158,7 +149,7 @@ func (c *Cache) cachePatch(patchUrl string) (string, error) {
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to fetch patch: %s", resp.Status)
+		return "", fmt.Errorf("failed to fetch %s: %s", patchUrl, resp.Status)
 	}
 
 	defer resp.Body.Close()
@@ -177,38 +168,53 @@ func (c *Cache) cachePatch(patchUrl string) (string, error) {
 	return patchPath, nil
 }
 
-type patchInfo struct {
+type patchMeta struct {
 	Requires   string   `json:"Requires"`
 	ServerData []string `json:"ServerData"`
 	// There are more fields in the actual JSON
 	// but we don't need them
 }
 
-func (c *Cache) patchInfo(version string) (*patchInfo, error) {
-	patchInfoPath := filepath.Join(c.path, fmt.Sprintf("patch_%s.json", version))
-	data, err := os.ReadFile(patchInfoPath)
+func (c *Cache) patchMeta(version string) (*patchMeta, error) {
+	path := filepath.Join(c.path, fmt.Sprintf("patch_%s.json", version))
+	data, err := os.ReadFile(path)
 	if err != nil {
-		patchInfoUrl := fmt.Sprintf("http://prbf2.cdn.ancientdev.com/patch_%s.json", version)
-		resp, err := http.Get(patchInfoUrl)
-		if err != nil {
-			return nil, err
-		}
-
-		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("failed to fetch patch_%s.json: %s", version, resp.Status)
-		}
-
-		defer resp.Body.Close()
-		data, err = io.ReadAll(resp.Body)
+		url := fmt.Sprintf(patchMetaUrlFmt, version)
+		data, err = httpGet(url)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	var info patchInfo
+	var info patchMeta
 	if err := json.Unmarshal(data, &info); err != nil {
 		return nil, fmt.Errorf("failed to decode patch_%s.json: %w", version, err)
 	}
 
 	return &info, nil
+}
+
+func httpGet(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch %s: %s", url, resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func filenameFromURL(s string) (string, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+
+	parts := strings.Split(u.Path, "/")
+	return parts[len(parts)-1], nil
 }
