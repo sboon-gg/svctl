cf8d0046195bdd6757a5904e4440a4e7fcb03042
cassius23 <emilekm@gmail.com>
2024-05-09 21:39:02 +0200

    Minimize interface

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,6 +1,11 @@
 package game
 
-import "time"
+import (
+	"context"
+	"io"
+	"os"
+	"time"
+)
 
 type Game interface {
 	Configs
@@ -10,54 +15,20 @@ type Game interface {
 }
 
 type Configs interface {
-	// Regsiter configuration file path
-	RegisterConfig(path string)
-
 	// Writes configuration file, storing original file.
 	// Stores origing only first time, then it will be ignored.
 	// Registers file if not already registered.
 	WriteConfig(path string, data []byte) error
 }
 
-type Status int
-
-const (
-	StatusUnknown Status = iota
-	StatusRunning
-	StatusStopped
-	StatusUpdating
-)
-
 type Process interface {
-	// Adopt running process.
-	// NOTE: not sure if this is needed since we can just
-	// store PID and use it to check if process is running.
-	// The intent is to be able to recover from daemon crash.
-	// Adopt(*os.Process) error
-
 	// Start game server process.
-	// Returns error if process is already running.
-	Start() error
-
-	// Stop game server process.
-	// If process is not running, returns nil.
-	Stop() error
-
-	// Checks if game server process is running.
-	// Only checks if process is running,
-	// to check if server is healthy use Status().
-	IsRunning() bool
-
-	// Returns game server status.
-	Status() (Status, error)
+	Start() (*os.Process, error)
 }
 
-// TODO: consider returning more information about the update:
-// new version, changed config files, etc.
 type Updater interface {
 	// Update game server.
-	// Returns error update is in progress.
-	Update() error
+	Update(ctx context.Context, output io.Writer) error
 }
 
 type ArtifactType int
@@ -78,12 +49,8 @@ type Artifact interface {
 
 	// Artifact type
 	Type() ArtifactType
-
-	// Whether the round the artifact belongs to is finished.
-	Finished() bool
 }
 
-// TODO: improve this interface
 type Artifacts interface {
 	Chatlogs() []Artifact
 	PRDemos() []Artifact
